bcb0644f690e27ac6b34db1e72a9d2a1118ff32a
Jérémy <jeremy.guilbert@stormshield.eu>
2023-04-02 01:02:06 +0200

    Add all images

--- futuramhordes/src/constant/characterList.ts
+++ futuramhordes/src/constant/characterList.ts
@@ -2,14 +2,242 @@ import type { Character } from "@/models/character";
 
 export const characterList: Array<Partial<Character>> = [
   {
-    character: "Bender",
-    fullName: "",
-    description: "Bender bender bender",
+    character: "Bender", //Intensive
+    fullName: "Bender Rodrigez",
+    description: "Robot tordeur de profession mais surtout doté d'un caractère cynique à tendance kleptomane et fumant des cigares.",
   },
   {
-    character: "Fry",
+    character: "Fry", //Heartgold
     fullName: "Philip J. Fry",
     description:
-      "Philip J. Fry, dit Fry, jeune livreur malchanceux de pizzas à New York, est accidentellement cryogénisé le 31 décembre 1999. Il se réveille mille ans plus tard, le 31 décembre 2999. New York est devenue New New York et bien des choses ont changé. Au centre de cryogénie, Turanga Leela, une « extraterrestre » cyclope, lui attribue le métier de livreur. Mais lorsque celle-ci veut lui poser l’implant correspondant à son nouveau métier, Fry s’enfuit. Il rencontre Bender Tordeur Rodriguez, un robot tordeur, dans une cabine à suicide (que Fry a confondu avec une cabine téléphonique). Après quelques péripéties, le professeur Hubert Farnsworth (qui est en fait l’arrière-arrière-arrière-… arrière-petit neveu de Fry) embauche Leela, Bender et Fry pour piloter le vaisseau de sa société de livraisons interstellaires « Planet Express ». Ils rencontrent alors leurs nouveaux collègues : le bureaucrate Hermes Conrad, la belle stagiaire Amy Wong et l’incompétent docteur (spécialisé dans la médecine humaine) John A. Zoïdberg. ",
+      "C'est un jeune adulte, bête, paresseux mais néanmoins volontaire.",
   },
+  {
+    character: "Zoidberg", //Kraky
+    fullName: "Dr Zoïdberg ",
+    description:
+      "Chirurgien (plutôt médiocre et pauvre) crustacé, d'une lointaine planète.",
+  },
+  {
+    character: "Agnew", //Renack
+    fullName: "Agnew",
+    description:
+      "Spiro Theodore Agnew (9 novembre 1918 - 17 septembre 1996) est vice-président pendant la première et l'actuelle présidence de Richard Nixon. Il est un corps sans tête, ce qui fait de lui l'opposé des têtes dans les bocaux. Il a été tué accidentellement par les écoféministes avec une voiturette de golf qui s'est emballée.",
+  },
+  {
+    character: "Amy", //Emy
+    fullName: "Amy Wang",
+    description:
+      "Fille d'une famille de milliardaires de la planète Mars. Grosse quand elle était petite, elle officie en tant que stagiaire, du même groupe sanguin que le professeur Farnsworth au cas où…",
+  },
+  {
+    character: "Calculon", //Seven
+    fullName: "Calculon",
+    description:
+      "Robot acteur, premier rôle de la série à l'eau de rose Tous mes circuits (All My Circuits).",
+  },
+  {
+    character: "Cinnamon", //SilAfyen
+    fullName: "Cinnamon",
+    description:
+      "Cinnamon est le poisson rouge utilisé par le professeur Ogden Wernstrom pour faire la démonstration de son invention, le scaphandre inversé, à l'Académie des inventeurs. Cinnamon peut aller chercher mais ne sait pas s'asseoir.",
+  },
+  {
+    character: "Clamps", //Mnemo
+    fullName: "Clamps",
+    description:
+      "Un robot de la mafia qui a des pinces à la place des mains et qui veut tout pincer !",
+  },
+  {
+    character: "Comtesse", //Cawina
+    fullName: "Comtesse de la Roca",
+    description:
+      "La comtesse de la Roca (12 septembre 2256 - 26 septembre 3000) était une riche Robot qui était aussi un yacht de classe 3. Elle a embarqué sur le Titanic où elle et Bender sont tombés amoureux.",
+  },
+  {
+    character: "Crushinator", //Kyliu
+    fullName: "Crushinator",
+    description:
+      "Crushinator est un robot qui a vécu sur la lune avec son père agriculteur hydroponique et ses sœurs robots. Elle est également un véhicule et peut être conduite comme une voiture.",
+  },
+  {
+    character: "Perceptron", //Salam
+    fullName: "Dr Perceptron",
+    description:
+      "Le Dr Perceptron est le médecin-chef de l'Institut Hal pour les robots criminellement fous.",
+  },
+  {
+    character: "Flexo", //Netto
+    fullName: "Flexo",
+    description:
+      "Robot de la même série que Bender. Il ressemble comme deux gouttes d'eau à Bender mais il a un petit bouc en métal sous le menton.",
+  },
+  {
+    character: "Guenter", //Ormax
+    fullName: "Guenter",
+    description:
+      "Un singe rendu intelligent grâce à un chapeau fait par le professeur",
+  },
+  {
+    character: "Hermes", //Antofire
+    fullName: "Hermes Conrad",
+    description:
+      "Fonctionnaire chevronné se réclamant de culture jamaïcaine et expert en bureaucratie, ancien champion de limbo, il exerce la fonction de comptable.",
+  },
+  {
+    character: "Hypnotoad", //Solivane
+    fullName: "Hypnotoad",
+    description:
+      "Un crapaud qui hypnotise les gens pour qu'on l'aime",
+  },
+  {
+    character: "Iperpollo", //Dodo
+    fullName: "Iperpollo",
+    description:
+      "L'Hyper-Poulet, un simple extraterrestre venu d'un astéroïde reculé.",
+  },
+  {
+    character: "Kif", //Bigonoud
+    fullName: "Kif",
+    description:
+      "Second de Zapp Brannigan, c'est un petit extraterrestre vert, timide et gentil. On pourrait le comparer à une sorte de grenouille humanoïde. Malgré son aspect chétif, il n'en reste pas moins très courageux.",
+  },
+  {
+    character: "Linda", //Dolc
+    fullName: "Linda van Schoonhoven",
+    description:
+      "Linda van Schoonhoven (née le 9 décembre 2970) est la co-présentatrice de Channel √2 News et de Good Morning, Earth.",
+  },
+  {
+    character: "Marianne", //Elara
+    fullName: "Marianne la fleuriste",
+    description:
+      "Zoidberg rencontre Marianne à son stand de fleurs, et elle s'intéresse à lui bien que Zoidberg dise qu'il sent mauvais. Cependant, comme Marianne est née sans odorat, elle n'a pas été affectée par l'odeur de Zoidberg.",
+  },
+  {
+    character: "Mom", //Renya
+    fullName: "Mom",
+    description:
+      "Présidente de la Mom’s Friendly Robot Company. Humaine, possessive, manipulatrice et sans cœur. Sous ses airs de gentille grand-mère dodue, elle est en fait une femme d'affaire rachitique sans scrupule. Elle est l'ex du professeur Farnsworth.",
+  },
+  {
+    character: "Morbo", //Valerant
+    fullName: "Morbo",
+    description:
+      "Extraterrestre à l'encéphale hypertrophié. Co-présentateur du journal télévisé. Ne manque jamais de faire des remarques cyniques et belliqueuses à l'encontre des humains, essayant d'aider les siens à envahir la Terre par sous entendus.",
+  },
+  {
+    character: "Morgan", //Themis
+    fullName: "Morgan Proctor",
+    description:
+      "Elle est bureaucrate de grade 19 et s'est choisie pour diriger Planet Express après qu'un collègue bureaucrate, Hermes Conrad, a échoué à une inspection bureaucratique officielle menée par elle.",
+  },
+  {
+    character: "Winkles", //Katopris
+    fullName: "Mr Winkles",
+    description:
+      "Un chat.",
+  },
+  {
+    character: "Nibbler", //ayesha
+    fullName: "Nibbler",
+    description:
+      "Petit animal de compagnie de Leela, âgé de plus de mille ans et qui a provoqué la venue de Fry dans le troisième millénaire, de manière à ce qu'il vainque les cerveaux.",
+  },
+  {
+    character: "Nixon", //Zerah
+    fullName: "Nixon",
+    description:
+      "Elle devient présidente des États-Unis de la Terre.",
+  },
+  {
+    character: "Farnsworth", //Biosha
+    fullName: "Professeur Farnsworth",
+    description:
+      "Il est le propriétaire de la société Planet Express et inventeur farfelu, âgé de 160 ans, prétendant n'en avoir que 150. Il est le créateur des robots qui sont maintenant produits dans l'usine de M'man.",
+  },
+  {
+    character: "Wernstrom", //Cavy
+    fullName: "Professeur Wernstrom",
+    description:
+      "L'ennemi juré du Pr Farnsworth. Leur différend remonte à l'époque où Wersnstrom était élève du professeur à l'Université martienne et que ce dernier lui attribua un A- ; Wernstrom jura alors de se venger même si cela devait lui prendre « cent ans ».",
+  },
+  {
+    character: "Rat", //Peutre
+    fullName: "Rat Man",
+    description:
+      "Rat Man est un ami d'Alkazar et fréquente la même personne depuis 3001. Cependant, ils ne sont pas encore mariés.",
+  },
+  {
+    character: "Devil", //EDF
+    fullName: "Robot Devil",
+    description:
+      "Il s'agit du diable de la religion robotologique, il vit dans l'enfer des robots, sous un parc d'attraction abandonné du New Jersey. Il passe des pactes et torture les méchants robots.",
+  },
+  {
+    character: "Elder", //arcan
+    fullName: "Robot Elder",
+    description:
+      "Les Robot Elders sont un groupe de cinq robots qui gouvernent la planète Chapek 9. Ils forment un gouvernement corrompu. Ils savent que les humains ne sont pas une menace pour les robots, mais ils diffusent quand même de la propagande anti-humaine parmi les robots de Chapek 9. Ils ont été sculptés dans des météorites par les fondateurs des robots.",
+  },
+  {
+    character: "Hedonis", //zomzom
+    fullName: "Robot Hedonis",
+    description:
+      "Il s'agit d'un robot inspiré d'un ancien hédoniste romain, avec l'amélioration notable d'être son propre kline.",
+  },
+  {
+    character: "Sally", //Bocky
+    fullName: "Sally",
+    description:
+      "Sally est une orpheline de l'orphelinat de sécurité minimale de Cookieville. Elle fait partie des orphelins que Bender a brièvement adoptés. Les autres orphelins se moquent parfois d'elle parce qu'elle a une oreille sur le visage et, apparemment, une queue. Sally affirme qu'elle a fait 100 saut à la corde d'affilée.",
+  },
+  {
+    character: "Smudge", //Dadouch
+    fullName: "Smudge Smudge",
+    description:
+      "Un chat.",
+  },
+  {
+    character: "Hawking", //Eragony
+    fullName: "Stephen Hawking's head",
+    description:
+      "Au 31e siècle, il est devenu une tête dans un bocal. Comme celui d'Al Gore, son bocal est alimenté par une fusée. Comme il ne peut toujours pas parler, son bocal agit comme un robot avec la même voix. Il a la capacité de tirer des lasers à partir de ses yeux, ce qui peut temporairement étourdir les gens, bien qu'il n'ait jamais su qu'il pouvait le faire jusqu'à ce qu'il l'ait réellement fait.",
+  },
+  {
+    character: "Brain", //Arendil
+    fullName: "The Big Brain",
+    description:
+      "Le Grand Cerveau est le chef de l'engeance cérébrale. C'est un cerveau massif qui vit dans l'Infosphère. Le Grand Cerveau a mené plusieurs attaques sur d'autres planètes en rendant leur population stupide, puis en détruisant la planète.",
+  },
+  {
+    character: "Thubanian", //Namelies
+    fullName: "Thubanian",
+    description:
+      "Un chat.",
+  },
+  {
+    character: "Tim", //hdmv
+    fullName: "Tinny Tim",
+    description:
+      "Tinny Tim (né en 2987) est un robot orphelin malchanceux qui a été programmé pour mendier, vendre de l'huile de vidange et écrire de jolies lettres à l'envers sur des panneaux. Il vend également le New New York Post.",
+  },
+  {
+    character: "Munda", //Jerry
+    fullName: "Turanga Munda",
+    description:
+      "La mère de Leela.",
+  },
+  {
+    character: "Zapp", //Malphas
+    fullName: "Zapp Brannigan",
+    description:
+      "Zapp est le commandant du vaisseau Nimbus de l'ordre démocratique des planètes. C'est un affabulateur et un égocentrique qui se veut séducteur. Il veut coucher avec Leela à chaque fois qu'il la voit.",
+  },
+  {
+    character: "Vanderbilt", //Kryy
+    fullName: "Boobs Vanderbilt",
+    description:
+      "Boobs Vanderbilt était une joueuse de poker, elle a été l'une des finalistes du 3009's Universal Poker Championship. Malheureusement, elle a connu le même sort que Tex Connecticut, lorsque deux cartes géantes sont tombées du plafond, les tuant tous les deux.",
+  }
 ];
